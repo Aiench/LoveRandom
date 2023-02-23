a5a55790922b532fe8700dce42211f73bb720d6b
Arina <arina.yench@bk.ru>
2023-02-23 10:39:09 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,3 @@
+const firstName = prompt('Напишите первое имя'), secondName = prompt('Напишите второе имя')
+const loveMatch= Math.floor(Math.random() * 100)
+alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${loveMatch}`)
